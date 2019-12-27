b5abc430595a6a2a39ad339c84a9457ef45b1be7
Guoxweii <alphaguoxiongwei@gmail.com>
2019-12-27 10:48:23 +0800

    增加测试按钮

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get "/" => "main#root", as: :root
-  get "abc" => "main#root"
+  match "abc" => "main#root", via: [ :delete, :put, :get, :patch ]
 end
